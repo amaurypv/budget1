ba266eecddbc52aff614cd786019e65987624924
amaurypv <amaurypv@gmail.com>
2023-10-04 22:03:24 -0600

    se agrego un ultimo put para cambiar valores entre sobres

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 const express=require('express')
 const app=express()
 const morgan=require('morgan')
@@ -21,8 +23,7 @@ app.get('/envelopes/:id',(req,res,next)=>{
         res.send('no se encuentra el id')
     }
   console.log(siid)  
-}
-)
+})
 
 app.get('/envelopes',(req,res,next)=>{
     res.json(envelopes)
@@ -41,12 +42,39 @@ app.put('/envelope/:id/:bud',(req,res,next)=>{
     const newbudget=req.params.bud
     const indiceId=envelopes.findIndex(x=>x.id==pathid)
     if(indiceId!=-1){
-        envelopes[indiceId].budget=newbudget
+        envelopes[indiceId].budget-=newbudget
         res.send(envelopes[indiceId])
     }else{
         res.send('no existe el id')
     }
+    console.log(envelopes[indiceId])
+})
 
+app.delete('/envelope/delete/:id',(req,res,next)=>{
+    const sobre=req.params.id
+    const indiceId=envelopes.findIndex(x=>x.id==sobre)
+    if(indiceId!=-1){
+        envelopes.splice(indiceId,1)
+        res.json({mensaje:`se eliminó el id`})
+    }else{
+        res.send('no se encontró el id')
+    }
+    console.log(`se eliminó el sobre de ${envelopes[indiceId][title]}`)
+})
+    
+app.post('/envelopes/switch/:from-:to/:qty',(req,res,next)=>{
+    const desde=req.params.from
+    const para=req.params.to
+    const cantidad=parseInt(req.params.qty)
+    const existedesde=envelopes.findIndex(x=>x.title==desde)
+    const existepara=envelopes.findIndex(y=>y.title==para)
+    if(existepara!=-1 && existedesde!=-1){
+        envelopes[existedesde].budget-=cantidad
+        envelopes[existepara].budget+=cantidad
+        res.json(envelopes[existepara])
+    }else{
+        res.send('no existe alguno de los sobres')
+    }
 })
 
 
