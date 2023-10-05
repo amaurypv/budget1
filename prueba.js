290c70de25d8d965839bea301ec7c546eb65b23f
amaurypv <amaurypv@gmail.com>
2023-10-05 15:53:29 -0600

    version final

--- prueba.js
+++
@@ -1,5 +0,0 @@
-const envelopes=[{'id':1,"budget":200,"title":"food"},{"id":2,"budget":300,"title":"clothes"},{"id":3,"budget":100,"title":"fun"}]
-const valor=3
-const nuevo=400
-const indiceId=envelopes.findIndex(x=>x.id==valor)
-console.log(envelopes[indiceId].budget)
